a71f7356a677025fbb151de79000afd9368be0ea
Mohammed Ali Baddah <mabaddah@gmail.com>
2022-11-14 07:06:14 +1100

    Added Richest Customer Wealth and tests

---
+++ lib/src/main/java/leetcode/RichestCustomerWealth.java
@@ -0,0 +1,23 @@
+package leetcode;
+
+//  Problem 1672
+
+public class RichestCustomerWealth {
+    public int maximumWealth(int[][] accounts) {
+        int maximum = 0;
+        int current = 0;
+
+        for (int i = 0; i < accounts.length; i++) {
+
+            if (current > maximum) {
+                maximum = current;
+            }
+            current = 0;
+            for (int j = 0; j < accounts[i].length; j++) {
+                current += accounts[i][j];
+            }
+        }
+
+        return maximum;
+    }
+}
