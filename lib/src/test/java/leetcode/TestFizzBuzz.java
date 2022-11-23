925e1cb44e05ae35796cec8de1132f60f22020d6
Mohammed Ali Baddah <mabaddah@gmail.com>
2022-11-24 06:17:29 +1100

    Added Fizz Buzz solution and tests

---
+++ lib/src/test/java/leetcode/TestFizzBuzz.java
@@ -0,0 +1,37 @@
+package leetcode;
+
+import org.junit.jupiter.api.Test;
+
+import java.util.ArrayList;
+import java.util.List;
+import static org.junit.jupiter.api.Assertions.*;
+
+public class TestFizzBuzz {
+
+    @Test
+    public void testFizzBuzzThree() {
+        FizzBuzz fizzBuzz = new FizzBuzz();
+        List<String> actual = fizzBuzz.fizzBuzzSolution(3);
+        List<String> expected = new ArrayList<String>();
+        expected.add("1");
+        expected.add("2");
+        expected.add("Fizz");
+        assertEquals(actual, expected);
+
+    }
+
+
+    @Test
+    public void testFizzBuzzFive() {
+        FizzBuzz fizzBuzz = new FizzBuzz();
+        List<String> actual = fizzBuzz.fizzBuzzSolution(5);
+        List<String> expected = new ArrayList<String>();
+        expected.add("1");
+        expected.add("2");
+        expected.add("Fizz");
+        expected.add("4");
+        expected.add("Buzz");
+        assertEquals(actual, expected);
+
+    }
+}
